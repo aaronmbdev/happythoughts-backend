1c58c7f73fb82b22275f6d01f36d709977edb4d7
Aaron Morales <aaron@midokura.com>
2021-12-10 19:41:50 +0100

    Primer commit

---
+++ src/main/java/com/kernelpanic/happythoughts/HappythougthsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kernelpanic.happythoughts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappythougthsBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappythougthsBackendApplication.class, args);
+    }
+
+}
